c1a636b102c6e96596050845528735eb5f9e859c
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2020-09-22 12:50:40 +0800

    初始化 aside 的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div>
     <Topnav />
     <div class="banner">
-      <h1>海子UI</h1>
-      <h2>一个基于 Vue 3 的 UI 框架</h2>
+      <h1>海子 UI</h1>
+      <h2>基于 Vue 3 的 UI 框架</h2>
       <p class="actions">
         <a href="">GitHub</a>
-        <a href="">开始</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -19,4 +19,26 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
