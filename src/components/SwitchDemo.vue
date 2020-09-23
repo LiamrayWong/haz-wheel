36e7055d004c6cf52d9a584f3efc33cfcde4ac24
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2020-09-23 17:53:57 +0800

    switch 结构优化

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,17 @@
 <template>
-  <Switch />
+  <Switch v-model:value="bool" />
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
 export default {
   components: {
     Switch,
+  },
+  setup() {
+    const bool = ref(true);
+    return { bool };
   },
 };
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,16 +1,21 @@
 <template>
-  <button @click="toggle" :class="{ checked: checked }"><span></span></button>
+  <button @click="toggle" :class="{ checked: value }">
+    <span></span>
+  </button>
+  <div>{{ value }}</div>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 export default {
-  setup() {
-    const checked = ref(false);
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value;
+      context.emit("update:value", !props.value);
     };
-    return { checked, toggle };
+    return { toggle };
   },
 };
 </script>
@@ -25,24 +30,35 @@ button {
   background: grey;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2/2;
-  transition: left 250ms;
-}
-button.checked {
-  background: blue;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus {
-  outline: none;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2/2;
+    transition: left 250ms;
+  }
+  &.checked {
+    background: #10a0ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 2px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 2px;
+      margin-left: -2px;
+    }
+  }
 }
 </style>
