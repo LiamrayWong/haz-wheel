2efe35d75690035ec0638b6214ba2eab10baf73f
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2020-11-09 09:37:13 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,7 +2,7 @@
   <div class="haz-tabs">
     <div ref="container" class="haz-tabs-nav">
       <div v-for="(t,index) in titles" :key="index"
-           :ref="el => {if(el) navItems[index] = el}" :class="{selected:t === selected}"
+           :ref="el => {if(t===selected) selectedItem = el}" :class="{selected:t === selected}"
            class="haz-tabs-nav-item" @click="select(t)">{{ t }}
       </div>
       <div ref="indicator" class="haz-tabs-nav-indicator"></div>
@@ -28,16 +28,14 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem =ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = ()=>{
-      const divs = navItems.value;
-      const result = divs.filter((div) => div.classList.contains('selected'))[0];
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {left:left1} = container.value.getBoundingClientRect()
-      const {left:left2} = result.getBoundingClientRect()
+      const {left:left2} = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -55,7 +53,7 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    return {defaults, titles, select, navItems, indicator, container};
+    return {defaults, titles, select, selectedItem, indicator, container};
   }
 };
 </script>
